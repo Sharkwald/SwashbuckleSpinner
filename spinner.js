af1fce412f53ef69bd3b2cd35e28d6e3eac585e9
Owen Morgan-Jones <owen.morgan-jones@civil.lmco.vom>
2015-03-27 15:07:01 +0000

    fixed easing, and put in a nice reset such that it's resuable without a hard refresh

--- spinner.js
+++ spinner.js
@@ -1,25 +1,31 @@
+var theSpinner, theTimer;
+
 function debug(message) {
   $("#debug").text(message);
 }
 
-function randomSelect() {
+function endTheSpin() {
+  $("#image").removeClass('spinning');
   var rotator = Math.floor(Math.random() * 360);
-  $(".image").css("text-indent", 0);
-  $(".image").animate({  textIndent: rotator + "px" }, { step : function (now) {
+  $("#image").css("text-indent", 0);
+  $("#image").animate({  textIndent: rotator + "px" }, { step : function (now) {
     $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
   }, duration: 2000, easing: "easeOutQuad" });
 }
 
-function doTheSpin() {
-  var theSpinner = $(".image");
+function resetSpinner() {
+  clearTimeout(theTimer);
   theSpinner.removeClass('spinning');
-  theSpinner.toggleClass('spinning');
-  setTimeout(function () {
-    $(".image").toggleClass('spinning');
-    randomSelect();
-  }, 3000);
+  theSpinner.removeAttr('style');
+}
+
+function doTheSpin() {
+  resetSpinner();
+  theSpinner.addClass('spinning');
+  theTimer = setTimeout(endTheSpin, 3000);
 }
 
 $(document).ready(function ($) {
+  theSpinner = $("#image");
   $("#go").click(doTheSpin);
 });
