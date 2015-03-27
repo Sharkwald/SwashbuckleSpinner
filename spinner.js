1e19bfba9af3fa94b9c4dc239cda1a1eb8d8a2c6
Owen Morgan-Jones <owen.morgan-jones@civil.lmco.vom>
2015-03-27 11:23:42 +0000

    chaining in a steady rotate and then a random select, though the easing is bobbins

--- spinner.js
+++ spinner.js
@@ -1,10 +1,22 @@
-function doTheSpin(e) {
-  var theSpinner = $(".image");
-  theSpinner.toggleClass('spinning');
-}
-
 function debug(message) {
   $("#debug").text(message);
+}
+
+function randomSelect() {
+  var rotator = Math.floor(Math.random() * 360);
+  $(".image").css("text-indent", 0).animate({  textIndent: rotator + "px" }, { step : function (now) {
+    $(this).css('-webkit-transform', 'rotate(' + now + 'deg)');
+  }, duration: 2000 }, "linear");
+}
+
+function doTheSpin() {
+  var theSpinner = $(".image");
+  theSpinner.removeClass('spinning');
+  theSpinner.toggleClass('spinning');
+  setTimeout(function () {
+    $(".image").toggleClass('spinning');
+    randomSelect();
+  }, 3000);
 }
 
 $(document).ready(function ($) {
